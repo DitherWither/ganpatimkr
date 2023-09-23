2f595e9128f16dbb569b6cf5a2962c327cc49fef
Vardhan Patil (DitherWither) <vardhanpatil@proton.me>
2023-09-23 17:29:44 +0530

    add code to draw Y

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -7,16 +7,35 @@ void Renderer::draw(
     this->height = height;
 
     drawBackground(cr);
-    moveToCenter(cr);
 
-    cr->rel_move_to(-160.0, -20.0);
     drawD(cr);
+
+    drawY(cr);
 }
 
-void Renderer::drawD(const Cairo::RefPtr<Cairo::Context>& cr) {
+void Renderer::drawD(const Cairo::RefPtr<Cairo::Context>& cr)
+{
+    moveToCenter(cr);
+    cr->rel_move_to(-160.0, -20.0);
     colorScheme.d.setAsCurrent(cr);
-    cr->rel_curve_to(10, -10, 40, -10, 60, 40);
+    cr->rel_line_to(60, 40);
+    // cr->rel_curve_to(10, -10, 40, -10, 60, 40);
     cr->rel_curve_to(20, -80, -80, -90, -60, -40);
+    cr->stroke();
+}
+
+void Renderer::drawY(const Cairo::RefPtr<Cairo::Context>& cr)
+{
+    moveToCenter(cr);
+    cr->rel_move_to(-130.0, -60.0);
+    colorScheme.y.setAsCurrent(cr);
+    cr->rel_curve_to(110, 30, 40, 180, 10, 210);
+    cr->stroke();
+
+    // back to src
+    moveToCenter(cr);
+    cr->rel_move_to(10, -80);
+    cr->rel_curve_to(-90, 60, 0, 100, -120, 250);
     cr->stroke();
 }
 
