61c6bce2863511c2b01bdc4c65b67520f0e5dbe4
Saumya Patil <saumyasachinpatil@gmail.com>
2023-09-23 17:29:44 +0530

    add code to draw Y

--- src/Renderer.hpp
+++ src/Renderer.hpp
@@ -13,5 +13,6 @@ private:
 
     void drawBackground(const Cairo::RefPtr<Cairo::Context>& cr);
     void drawD(const Cairo::RefPtr<Cairo::Context>& cr);
+    void drawY(const Cairo::RefPtr<Cairo::Context>& cr);
     void moveToCenter(const Cairo::RefPtr<Cairo::Context>& cr);
 };
