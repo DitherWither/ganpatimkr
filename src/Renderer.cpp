d5c0fce3f90526d071866203220984ebe5fb1dfb
Vardhan Patil (DitherWither) <vardhanpatil@proton.me>
2023-09-29 14:32:13 +0530

    create p

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -37,8 +37,11 @@ void Renderer::drawY(const Cairo::RefPtr<Cairo::Context>& cr)
     moveToCenter(cr);
     cr->rel_move_to(-100.0, -50.0);
     cr->rel_line_to(88, 132);
+
     cr->rel_move_to(20, -132);
     cr->rel_line_to(-50, 88);
+
+    // TODO curve the bottom end of y;
     cr->stroke();
 }
 
@@ -49,9 +52,10 @@ void Renderer::drawP(const Cairo::RefPtr<Cairo::Context>& cr)
     moveToCenter(cr);
     cr->rel_move_to(40, -40);
     cr->rel_line_to(-50, 88);
-    
+    cr->rel_move_to(50, -88);
+
+    cr->rel_curve_to(40, -10, 80,  80, -35,  60);
     cr->stroke();
-    // cr->rel_move_to();
 };
 
 void Renderer::drawMukut(const Cairo::RefPtr<Cairo::Context>& cr)
@@ -67,6 +71,12 @@ void Renderer::drawMukut(const Cairo::RefPtr<Cairo::Context>& cr)
 
     cr->rel_move_to(-70, -20);
     cr->rel_line_to(60, 0);
+    
+    cr->rel_move_to(-10, -20);
+    cr->rel_line_to(0, -20);
+
+    cr->rel_curve_to(0, -30, -40, -30, -40, 0);
+    cr->rel_line_to(0, 20);
     
     cr->stroke();
 }
