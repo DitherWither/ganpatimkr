15151096f5b7a24c7b4be2bffa3a54c962765187
Saumya Patil <saumyasachinpatil@gmail.com>
2023-09-29 17:16:17 +0530

    Add more info to readme

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include "MainWindow.hpp"
-#include <adwaita.h>
 #include <gtkmm.h>
+
+#define USE_LIBADWAITA
+
+#ifdef USE_LIBADWAITA
+#include <adwaita.h>
+#endif
 
 int main(int argc, char* argv[])
 {
@@ -9,8 +14,10 @@ int main(int argc, char* argv[])
     // Makes the application use libadwaita theme instead of the default one
     // we are not using libadwaita in the main application, but this is needed
     // for dark mode support.
+#ifdef USE_LIBADWAITA
     adw_style_manager_set_color_scheme(
         adw_style_manager_get_default(), ADW_COLOR_SCHEME_DEFAULT);
+#endif
 
     return app->make_window_and_run<MainWindow>(argc, argv);
 }
